ffecb167538be8630ef8b54d9c24b395e74f66e7
ritikgupta2109 <ritik.gupta_cs19@gla.ac.in>
2022-05-24 00:50:06 +0530

    Updates

---
+++ precache-manifest.c44876d7d423051196efdd1a5b123f7c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a787544c108bc74453fc6f0e877c7c90",
+    "url": "/Model_Portfolio_Template/index.html"
+  },
+  {
+    "revision": "3a17d7c21eeebd5250a3",
+    "url": "/Model_Portfolio_Template/static/css/main.022eb82c.chunk.css"
+  },
+  {
+    "revision": "c6b47daaacefce5298a3",
+    "url": "/Model_Portfolio_Template/static/js/2.7303e263.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/Model_Portfolio_Template/static/js/2.7303e263.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a17d7c21eeebd5250a3",
+    "url": "/Model_Portfolio_Template/static/js/main.f067242c.chunk.js"
+  },
+  {
+    "revision": "0671f12b59bdfc905a39",
+    "url": "/Model_Portfolio_Template/static/js/runtime-main.03e84544.js"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/Model_Portfolio_Template/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "e87a287d8d391e85aa81b71087963188",
+    "url": "/Model_Portfolio_Template/static/media/ritikgupta.e87a287d.jpg"
+  },
+  {
+    "revision": "4261051984aec1618e096beb3117a676",
+    "url": "/Model_Portfolio_Template/static/media/ritikgupta_resume.42610519.pdf"
+  }
+]);
